f8bf01ef92189f705a631424b1f7509c62a00957
Nishant Ketu <69218488+ketunishant@users.noreply.github.com>
2021-07-21 15:36:23 +0530

    Finally selenium Test project

--- index.js
+++ index.js
@@ -1,44 +1,95 @@
-const { Builder, By, Key, util } = require("selenium-webdriver");
+const {Builder, By, Key, until} = require("selenium-webdriver");
+const fs = require("fs");
+const path = require("path");
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
 
-async function example() {
-  let driver = await new Builder().forBrowser("firefox").build();
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+(async function(){
+    let driver = await new Builder().forBrowser("chrome").build();
+    let urlArr = [
+        "http://thesparksfoundation.sg", 
+        "http://thesparksfoundationsingapore.org/about/vision-mission-and-values/",
+        "http://www.thesparksfoundationsingapore.org/contact-us/",
+        "http://www.thesparksfoundationsingapore.org/programs/student-scholarship-program/",
+        "http://www.thesparksfoundationsingapore.org/about/executive-team/"
+    ]
 
-  // await driver.findElement(By.name('q')).sendKeys("Selenium",Key.RETURN)
-  // await driver.get("https://lambdatest.github.io/sample-todo-app/");
-  await driver.get("https://www.sparksfoundation.org/");
+    try{
+        await driver.manage().window().maximize()
+        
+        
+        await driver.get(urlArr[0]);
 
-  //for title
-  var title = await driver.getTitle();
-  console.log("Title is:", title);
+        let logo = await driver.findElement(By.css("h1 a img")).isDisplayed();
+        if (logo){
+            console.log("Website Logo 👍 exists!")
+        }
 
-  //for url
+        let footer = await driver.findElement(By.css(".copyright-wthree")).isDisplayed();
+        if (footer){
+            console.log("Exists Website Footer   👉  " +footer)
+        }
 
-  var url = await driver.getCurrentUrl() ;
-  console.log("Url :" + url)
+        let pageTitle1 = await driver.getTitle();
+        if (pageTitle1 === "The Sparks Foundation | Home"){
+            console.log("Page Title is 👍 correct!")
+        }
 
+        await driver.get(urlArr[1])
+        
+        let pageTitle2 = await driver.getTitle();
+        if (pageTitle2 === "The Sparks Foundation | Home"){
+            console.log("Page Title is👍 correct!" + " 👉 "+pageTitle2)
+        }
 
- //for pagesource
-//   var PageSource = await driver.getPageSource() ;
-// console.log("PageSource :" +PageSource)
+        let heading1 = await driver.findElement(By.css(".inner-tittle-w3layouts")).getText();
+        if (heading1 === "Vision, Mission And Values"){
+            console.log("Heading 1 : Heading is 👍 Correct! 👉" +heading1)
+        }
 
+        await driver.get(urlArr[2])
+        let heading2 = await driver.findElement(By.css(".inner-tittle-w3layouts")).getText();
+        if (heading2 === "Contact Us"){
+            console.log("Heading 2:  Heading is  👍 Correct!  👉" +heading2)
+        }
 
-// var p = await driver.findElement()
-// console.log(p) 
+        let addressDiv = await driver.findElement(By.css(".agile_banner_bottom_grids > div:nth-child(2)")).isDisplayed();
+        if (addressDiv){
+            console.log(" Displaying properly Address Div")
+        }
 
-// var e = await driver.findElements()
-// console.log(e) 
+        await driver.get(urlArr[3])
+        let heading3 = await driver.findElement(By.css(".inner-tittle-w3layouts")).getText();
+        if (heading3 === "Student Scholarship Program"){
+            console.log(" Heading 3: Heading is 👍  Correct!  👉 " +heading3)
+        }
 
-var g =await driver.getSession()
-console.log("Session :" +g)
+        let supportDiv = await (await driver.findElement(By.css(".tittle-agileits-w3layouts ~ div:nth-child(4) .media-body")).findElements(By.css("p"))).length;
+        if (supportDiv === 3){
+            console.log("Existing of Support div 👍 ")
+        }
 
-// var h =await driver.getCapabilities()
-// console.log(h)
+        await driver.get(urlArr[4])
+        let heading4 = await driver.findElement(By.css(".inner-tittle-w3layouts")).getText();
+        if (heading4 === "Executive Team"){
+            console.log("Heading 4: Heading is 👍 Correct!  👉 " +heading4)
+        }
 
+        let founderImg = await driver.findElement(By.css(".profile-img")).isDisplayed();
 
-// console.log(i)
+        if (founderImg){
+            console.log("Visible ,Founder Image  👍");
+        }
 
-  //It is always a safe practice to quit the browser after execution
-  await driver.quit();
-}
+    }catch(e){
 
-example();
+        console.log(e.message);
+
+    }finally{
+
+        driver.quit();
+
+    }
+})()
--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,3 +1,5 @@
+/*  //How Selenium webdriver work?
+
 // const { Builder } = require("selenium-webdriver");
 
 // async function example() {
@@ -13,3 +15,51 @@
 // }
 
 // example();
+
+
+const { Builder, By, Key, util } = require("selenium-webdriver");
+
+async function example() {
+  let driver = await new Builder().forBrowser("firefox").build();
+
+  // await driver.findElement(By.name('q')).sendKeys("Selenium",Key.RETURN)
+  // await driver.get("https://www.sparksfoundation.org/");
+  
+
+  //for title
+  var title = await driver.getTitle();
+  console.log("Title is:", title);
+
+  //for url
+
+  var url = await driver.getCurrentUrl() ;
+  console.log("Url :" + url)
+
+
+ //for pagesource
+//   var PageSource = await driver.getPageSource() ;
+// console.log("PageSource :" +PageSource)
+
+
+// var p = await driver.findElement()
+// console.log(p) 
+
+// var e = await driver.findElements()
+// console.log(e) 
+
+var g =await driver.getSession()
+console.log("Session :" +g)
+
+// var h =await driver.getCapabilities()
+// console.log(h)
+
+
+// console.log(i)
+
+  //It is always a safe practice to quit the browser after execution
+  await driver.quit();
+}
+
+example();
+
+*/
